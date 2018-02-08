ff676ed1a17d3fc7ed28710cb05e1625d2caa2e4
shivasupraj <shivasupraj.p@gmail.com>
2018-02-07 19:49:21 -0600

    Update ThreeInOne.py

--- Chapter 3/ThreeInOne.py
+++ Chapter 3/ThreeInOne.py
@@ -1,52 +1,51 @@
-class multi_stack:
-    def __init__(self, stack_capacity):
-        self.stack_number = 3
-        self.val = [0] * self.stack_number * stack_capacity
-        self.stack_sizes = [0] * self.stack_number
-        self.stack_capacity = stack_capacity
+class three_in_stack:
+    def __init__(self, capacity):
+        self.stack_size = 3
+        self.stack_capacity = capacity
+        self.stack_sizes = [0] * self.stack_size
+        self.stack_array = [0] * self.stack_size * self.stack_capacity
 
-    def push(self, stack_number, value):
-        stack_number -= 1
-        if self.is_stack_full(stack_number):
-            raise Exception('Stack is Full')
-        self.val[self.index_of_top(stack_number)] = value
+    def push(self, stack_number, val):
+        if self.is_full(stack_number):
+            raise Exception('Stack {0} is Full', stack_number)
+        self.stack_array[self.get_index(stack_number)] = val
         self.stack_sizes[stack_number] += 1
 
+    def get_index(self, stack_number):
+        return stack_number * self.stack_capacity + self.stack_sizes[stack_number]
+
+    def is_full(self, stack_number):
+        return self.stack_capacity == self.stack_sizes[stack_number]
+
     def pop(self, stack_number):
-        stack_number -= 1
-        if self.is_stack_empty(stack_number):
-            raise Exception('Stack is Empty')
-        temp = self.val[self.index_of_top(stack_number)]
-        self.val[self.index_of_top(stack_number)] = 0
+        if self.is_empty(stack_number):
+            raise Exception('Cannot pop from the stack number {0}', stack_number)
+        temp = self.stack_array[self.get_index(stack_number)-1]
+        self.stack_array[self.get_index(stack_number)-1] = 0
         self.stack_sizes[stack_number] -= 1
         return temp
-    def peek(self, stack_number):
-        stack_number -= 1
-        return self.val[self.index_of_top(stack_number)-1]
 
-    def is_stack_full(self, stack_number):
-        if self.stack_sizes[stack_number] == self.stack_capacity:
-            return True
-        else:
-            return False
+    def is_empty(self, stack_number):
+        return self.stack_sizes[stack_number] == 0
 
-    def is_stack_empty(self, stack_number):
-        if self.stack_sizes[stack_number] == 0:
-            return True
-        else:
-            return False
+    def print(self):
+        for each in self.stack_array:
+            print(each)
 
-    def index_of_top(self, stack_number):
-        offset = (stack_number) * self.stack_capacity + self.stack_sizes[stack_number]
-        return offset
 
-def three_in_one():
-    newstack = multi_stack(2)
-    print(newstack.stack_sizes)
-    newstack.push(1,1)
-    print(newstack.is_stack_empty(1))
-    print(newstack.peek(1))
-    print(newstack.pop(1))
-    print(newstack.is_stack_empty(1))
-three_in_one()
-
+stacks = three_in_stack(3)
+stacks.push(0,1)
+stacks.push(0,2)
+stacks.push(1,4)
+stacks.push(2,7)
+stacks.print()
+print(stacks.is_empty(0))
+print(stacks.is_empty(1))
+print(stacks.is_empty(2))
+print(stacks.pop(0))
+print(stacks.pop(0))
+print(stacks.pop(1))
+print(stacks.pop(2))
+print(stacks.is_empty(0))
+print(stacks.is_empty(1))
+print(stacks.is_empty(2))
